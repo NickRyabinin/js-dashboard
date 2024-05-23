efe8573e5bfab60014a077e1c006594bba31ae6a
Николай <rnn2002@mail.ru>
2024-05-23 18:06:06 +0300

    Modify main.js - make some code refactoring

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,18 +1,14 @@
 import { getWeatherWithAJAX, getWeatherWithFetch } from "./weather.js";
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', setEventListeners);
+
+function setEventListeners() {
   const formAjax = document.getElementById('form-ajax');
   const formFetch = document.getElementById('form-fetch');
 
   formAjax.addEventListener('submit', function (event) {
     event.preventDefault();
     getWeather(event);
-  });
-
-  formAjax.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-      getWeather(event);
-    }
   });
 
   formAjax.addEventListener('click', function (event) {
@@ -26,26 +22,18 @@ document.addEventListener('DOMContentLoaded', function () {
     getWeather(event);
   });
 
-  formFetch.addEventListener('keyup', function (event) {
-    if (event.key === 'Enter') {
-      getWeather(event);
-    }
-  });
-
   formFetch.addEventListener('click', function (event) {
     if (event.target.classList.contains('submit')) {
       getWeather(event);
     }
   });
-});
+}
 
 function getWeather(event) {
   const formId = event.target.closest('form').id;
-  if (event.type === "click" || event.key === "Enter") {
-    if (formId === "form-ajax") {
-      getWeatherWithAJAX();
-    } else if (formId === "form-fetch") {
-      getWeatherWithFetch();
-    }
+  if (formId === "form-ajax") {
+    getWeatherWithAJAX();
+  } else if (formId === "form-fetch") {
+    getWeatherWithFetch();
   }
 }
