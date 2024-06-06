47d385a2672727db86d1879d7550d0faf0b062ba
Николай <rnn2002@mail.ru>
2024-06-06 18:49:20 +0300

    Modify validator.js - make "ёЁ" be valid

--- src/scripts/validator.js
+++ src/scripts/validator.js
@@ -4,7 +4,7 @@ function validateInput(inputValue) {
   if (inputValue.trim() === '') {
       showMessage('Поле не должно быть пустым');
       return false;
-  } else if (/[^a-zA-Z0-9а-яА-Я\-_., ]/.test(inputValue)) {
+  } else if (/[^a-zA-Z0-9а-яА-ЯёЁ\-_., ]/.test(inputValue)) {
       showMessage('Введены недопустимые символы');
       return false;
   }
